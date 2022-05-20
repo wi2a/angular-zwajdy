0c0016fbec99017b0b3f4b09e712a87ccd2ee3d5
wi2a <sysweb@outlook.com>
2022-05-20 19:43:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zwajdy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zwajdy)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
+
+/*
+export class ProductAlertsComponent implements OnInit {
+  @Input() product!: Product;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+*/
+
